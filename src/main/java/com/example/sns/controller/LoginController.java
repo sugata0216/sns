0977dbfe65f2f3ad87871076dd729b9ce2617ecd
sugata <k.sugata.sys24@morijyobi.ac.jp>
2026-08-05 16:51:47 +0900

    リダイレクト先の修正

--- src/main/java/com/example/sns/controller/LoginController.java
+++ src/main/java/com/example/sns/controller/LoginController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/login";
+    }
+
     @GetMapping("/login")
     public String login() {
         return "login";
